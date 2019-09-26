be5ee4ebb6b0933d2566ccddacce9bc6b14a5f81
Kirill Müller <krlmlr+r@mailbox.org>
2019-09-26 21:08:40 +0200

    Import, analyze and export

---
+++ README.md
@@ -0,0 +1,21 @@
+# import-json
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of import-json is to demonstrate import of a JSON file.
+
+## Prerequisites
+
+- Place the `Todos Registros.json` file in the root directory of the project.
+- Make sure you have opened the project by double-clicking the `import-json.Rproj` file.
+
+## Code
+
+Run `import.R` to:
+
+- load the data
+- analyze the internal structure and extract a flat data frame
+- compute counts by group
+- display an exploratory plot
+- save the data as an Excel file
---
+++ import.R
@@ -0,0 +1,52 @@
+# Run once if necessary:
+#
+# install.packages(c("tidyverse", "jsonlite", "writexl"))
+
+library(tidyverse)
+library(jsonlite)
+library(writexl)
+
+# Read in with `simplifyVector = TRUE`, this flattens as much
+# as possible
+json <- read_json("Todos Registros.json", simplifyVector = TRUE)
+
+# Review structure
+names(json)
+str(json)
+
+# json$value is a data frame! Convert to a tibble for nicer output.
+data <- as_tibble(json$value)
+data
+
+# Elementary statistics
+data %>%
+  count(UfPessoaNacional)
+
+data %>%
+  count(MoedaOperacao) %>%
+  arrange(-n)
+
+data %>%
+  count(Ano)
+
+data %>%
+  filter(UfPessoaNacional != "", MoedaOperacao == "EUR") %>%
+  count(Ano)
+
+data %>%
+  filter(UfPessoaNacional != "", MoedaOperacao == "EUR") %>%
+  count(UfPessoaNacional)
+
+data %>%
+  filter(UfPessoaNacional != "", MoedaOperacao == "EUR") %>%
+  count(UfPessoaNacional)
+
+# Exploratory plot
+data %>%
+  filter(UfPessoaNacional != "", MoedaOperacao == "EUR") %>%
+  ggplot(aes(x = UfPessoaNacional, y = ValorOperacao)) +
+  scale_y_log10() +
+  geom_boxplot()
+
+# Save the data as Excel
+writexl::write_xlsx(data, "data.xlsx")
